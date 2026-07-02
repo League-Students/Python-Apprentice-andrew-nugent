803e7d045d367d848a9b455dcc2c0ce2a1d3bdab
League Student <student@jointheleague.org>
2026-07-02 13:28:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/40_Loops/30_Loop_with_Turtle.py
+++ lessons/10_Turtles/40_Loops/30_Loop_with_Turtle.py
@@ -11,7 +11,10 @@ In this program, use a loop to draw a regular pentagon (5-sided shape) with Tina
 
 uid: BpGnQq64
 name: Loop With Turtle
-
+"""
+import turtle
+tina = turtle.Turtle()
+tina
 
 for i in range(4):
     tina.forward(100)
