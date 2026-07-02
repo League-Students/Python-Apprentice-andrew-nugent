812204f9e1fc7851f5a972b07def5445e4871f19
League Student <student@jointheleague.org>
2026-07-02 13:38:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/50_Variables_and_Functions/20_Efficient_Turtle.py
+++ lessons/10_Turtles/50_Variables_and_Functions/20_Efficient_Turtle.py
@@ -20,14 +20,13 @@ def draw_polygon(sides):
 
     angle =360/sides                             # Calculate angle from number of sides
     
-    for i in range(sides):                 # Loop through the number of sides
+    for i in range(4):                 # Loop through the number of sides
         tina.forward(100)
-        tina.left(90)
+        tina.left(angle)
                                       # Move tina forward by the forward distance
         ...                              # Turn tina left by the left turn
 
-draw_polygon(...)                        # Draw a square
-
+draw_polygon()                        # Draw a square
 ...                                      # Move tina to another spot on the screen
 
 draw_polygon(...)                        # Draw a pentagon
