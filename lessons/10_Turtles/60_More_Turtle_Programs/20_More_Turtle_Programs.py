8667ce7c29e541d7ba184a86f8315378115217dd
League Student <student@jointheleague.org>
2026-07-07 12:28:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/60_More_Turtle_Programs/20_More_Turtle_Programs.py
+++ lessons/10_Turtles/60_More_Turtle_Programs/20_More_Turtle_Programs.py
@@ -39,7 +39,7 @@ screen.setup(width=600, height=600)
 # Create a turtle and set its shape to the custom GIF
 t = turtle.Turtle()
 
-set_turtle_image(t, "emoji")
+set_turtle_image(t, "emoji.png")
 
 t.penup()   # Prevent drawing when moving
 t.speed(3)  # Set a moderate speed
