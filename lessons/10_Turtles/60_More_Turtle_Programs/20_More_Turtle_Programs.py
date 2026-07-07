b3ea51ef3b845d561ffd8e25894ab36024e29ebd
League Student <student@jointheleague.org>
2026-07-07 12:27:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/60_More_Turtle_Programs/20_More_Turtle_Programs.py
+++ lessons/10_Turtles/60_More_Turtle_Programs/20_More_Turtle_Programs.py
@@ -8,12 +8,12 @@ directory ) and moves to the corners of the screen in a square pattern.
 
 import turtle
 import random
-def set_turtle_image(turtle,pikachu):
+def set_turtle_image(turtle,emoji):
     """Set the turtle's shape to a custom image."""
 
     from pathlib import Path                        # Import Path from pathlib module
     image_dir = Path(__file__).parent.parent / "images"    # Define the directory containing images
-    image_path = str(image_dir / pikachu)        # Create the full path to the image file
+    image_path = str(image_dir /emoji)        # Create the full path to the image file
 
     screen = turtle.getscreen()                     # Get the turtle's screen
     screen.addshape(image_path)                     # Register the image as a shape
