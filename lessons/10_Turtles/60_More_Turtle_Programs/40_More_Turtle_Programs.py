6b28fed6c33ae951e6a4c6e0da8e7af71d68d1a0
League Student <student@jointheleague.org>
2026-07-07 12:41:04 -0700

    Automated commit

--- lessons/10_Turtles/60_More_Turtle_Programs/40_More_Turtle_Programs.py
+++ lessons/10_Turtles/60_More_Turtle_Programs/40_More_Turtle_Programs.py
@@ -19,7 +19,7 @@ def set_turtle_image(turtle,emoji):
     """Set the turtle's shape to a custom image."""
 
     from pathlib import Path                        # Import Path from pathlib module
-    image_dir = Path(__file__).parent. / "images"    # Define the directory containing images
+    image_dir = Path(__file__).parent.parent / "images"    # Define the directory containing images
     image_path = str(image_dir /emoji)        # Create the full path to the image file
 
     screen = turtle.getscreen()                     # Get the turtle's screen
