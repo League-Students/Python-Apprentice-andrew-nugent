4b445cb530b082667d3b588792fc31d94fba9b63
League Student <student@jointheleague.org>
2026-07-07 13:14:04 -0700

    Automated commit

--- lessons/10_Turtles/70_Projects/10_LeagueBot.py
+++ lessons/10_Turtles/70_Projects/10_LeagueBot.py
@@ -23,8 +23,6 @@ t.penup()   # Prevent drawing when moving
 t.speed(3)  # Set a moderate speed
 
 # Move the turtle to each corner of the screen in a square pattern
-for x, y in [(200, 200), (200, -200), (-200, -200), (-200, 200)]:
-    t.goto(x, y)
 
 turtle.exitonclick() 
 
