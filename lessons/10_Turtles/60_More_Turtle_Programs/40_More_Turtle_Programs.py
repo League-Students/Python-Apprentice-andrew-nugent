ebb6a9c5dfa6f0e82f6e913344bec724d429fbf7
League Student <student@jointheleague.org>
2026-07-07 12:40:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/60_More_Turtle_Programs/40_More_Turtle_Programs.py
+++ lessons/10_Turtles/60_More_Turtle_Programs/40_More_Turtle_Programs.py
@@ -46,7 +46,7 @@ screen.setup(width=600, height=600)
 # Create a turtle and set its shape to the custom GIF
 t = turtle.Turtle()
 
-set_turtle_image(t,".gif")
+set_turtle_image(t,"leaguebot_bolt.gif")
 
 t.penup()   # Prevent drawing when moving
 t.speed(3)  # Set a moderate speed
